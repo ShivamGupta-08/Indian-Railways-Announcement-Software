60bcd2f5f303d188b0217080398dc083298445c6
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2021-10-02 11:59:58 +0530

    Indian Railways Announcement Software 1.0

---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import pandas as pd
+from pydub import AudioSegment
+from gtts import gTTS
+
+def textToSpeech(text,filename):
+    mytext= str(text)
+    language = 'hi'
+    myobj = gTTS(text= mytext,lang=language,slow=False)
+    myobj.save(filename)
+    
+
+def mergeAudios(audios):
+    combine = AudioSegment.empty()
+    for audio in audios:
+        combine+=AudioSegment.from_mp3(audio)
+    return combine 
+    
+
+def generateSkeleton():
+    #1 generate kripya dyan dijye
+    audio = AudioSegment.from_mp3('railway.mp3')
+    start  = 88000
+    finish = 90200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("1_hindi.mp3",format="mp3")
+    #2 is from city
+
+
+    #3 generate se chalkar
+    start  = 91000
+    finish = 92200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("3_hindi.mp3",format="mp3")
+
+    #4 is via city
+
+    #5 Generate  se raste
+    start  = 94000
+    finish = 95000
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("5_hindi.mp3",format="mp3")
+
+    #6 is to city
+
+    #7 generate ko jaane wali gaddi sakhya
+    start  = 96000
+    finish = 98900
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("7_hindi.mp3",format="mp3")
+
+    # 8 train no. and name
+
+    #9 is kuch hi samay me platfrom sankhya  
+    start  = 105500
+    finish = 108200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("9_hindi.mp3",format="mp3")
+
+    # 10 is platform no
+
+    # 11 pe aa rahi hai 
+    start  = 109000
+    finish = 112250
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("11_hindi.mp3",format="mp3")
+   
+
+def generateAnnouncment(filename):
+    df = pd.read_excel(filename)
+    print(df)
+    for index,item in df.iterrows():
+        # 2 - Generate from-city
+        textToSpeech(item['from'], '2_hindi.mp3')
+        # 4 - Generate via-city
+        textToSpeech(item['via'], '4_hindi.mp3')
+        # 6 - Generate to-city
+        textToSpeech(item['to'], '6_hindi.mp3')
+        # 8 - Generate train no and name
+        textToSpeech(item['train_no']+" "+item['train_name'], '8_hindi.mp3')
+        # 10 - Generate platform number
+        textToSpeech(item['platform'], '10_hindi.mp3')
+        audios = [f"{i}_hindi.mp3" for i in range(1,12)]
+
+        announcment = mergeAudios(audios)
+        announcment.export(f"announcement_{item['train_no']}_{index+1}.mp3",format="mp3")
+
+if __name__ == '__main__':
+    print("Generating Skeleton...")
+    generateSkeleton()
+    print("Now Generating Announcment...")
+    generateAnnouncment("announce_hindi.xlsx")
